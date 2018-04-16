453e61f881cf9843d4248a9073742244cd78b5a6
Simillo Nakai <simillo.lemaf@gmail.com>
2018-04-16 10:54:17 -0300

    Vertical align

--- Interface/src/App.vue
+++ Interface/src/App.vue
@@ -10,6 +10,12 @@ export default {
 </script>
 
 <style lang='scss'>
+
+html, body {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,9 +24,20 @@ export default {
 }
 
 .box {
-  width: 400px;
-  min-height: 400px;
-  border: 1px solid;
-  margin: 60px auto;
+  display: table;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  .middle-box {
+    display: table-cell;
+    vertical-align: middle;
+    .inner-box {
+      border: 1px solid #000;
+      width: 400px;
+      min-height: 500px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
 }
 </style>
--- Interface/src/components/Home.vue
+++ Interface/src/components/Home.vue
@@ -1,5 +1,7 @@
 <template lang='pug'>
   .box
+    .middle-box
+      .inner-box
 </template>
 
 <script>
