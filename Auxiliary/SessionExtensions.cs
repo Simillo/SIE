d190970af9da9b93ae799bad59a916cb0a06981a
Simillo Nakai <simillonakai@gmail.com>
2018-04-22 22:16:16 -0300

    prototipos

--- Auxiliary/SessionExtensions.cs
+++ Auxiliary/SessionExtensions.cs
@@ -23,7 +23,7 @@ namespace SIE.Auxiliary
 
         public static void Authenticate(this ISession session, Person person)
         {
-            session.SetString("_id", person.Id.ToString());
+            session.SetInt32("_id", person.Id);
             session.SetString("_name", person.Name);
             session.SetString("_cpf", person.Cpf);
             session.SetString("_email", person.Email);
