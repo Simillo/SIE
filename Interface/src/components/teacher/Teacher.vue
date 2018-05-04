da8e19038e3f3bd5a091c905a089a7dc57c0f66e
Simillo Nakai <simillo.lemaf@gmail.com>
2018-05-04 18:11:40 -0300

    navigation sidebar

--- Interface/src/components/teacher/Teacher.vue
+++ Interface/src/components/teacher/Teacher.vue
@@ -5,15 +5,18 @@
         md-toolbar.md-transparent(md-elevation='0')
           .logo
         md-list.sidebar-list
-          md-list-item.sidebar-item
+          md-list-item.sidebar-item(@click.prevent='changeState("my-rooms")')
+            .sidebar-icon
+              md-icon.md-size-2x list
+            span.md-list-item-text Minhas salas
+          md-list-item.sidebar-item(@click.prevent='changeState("new-room")')
             .sidebar-icon
               md-icon.md-size-2x book
             span.md-list-item-text Criar nova sala
-          md-list-item.sidebar-item
-            .sidebar-icon
-              md-icon.md-size-2x book
-            span.md-list-item-text Criar nova sala
-      md-app-content
+      md-app-content(v-if='currentState === "my-rooms"')
+        .asd teste
+      md-app-content(v-if='currentState === "new-room"')
+        .asd teste
   </div>
 </template>
 
@@ -24,11 +27,17 @@ import TeacherService from '../../services/TeacherService'
 export default {
   data () {
     return {
+      currentState: ''
     }
   },
   async created () {
     this.service = new TeacherService(this.$http)
     await this.service.load()
+  },
+  methods: {
+    changeState (newState) {
+      this.currentState = newState
+    }
   }
 }
 </script>
