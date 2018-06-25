8db616af354a8e3b065e38aa94eaf707b0401503
Simillo Nakai <simillo.lemaf@gmail.com>
2018-06-25 13:27:48 -0300

    cores

--- SIE/Dashboard/ActivitiesTeacher.cs
+++ SIE/Dashboard/ActivitiesTeacher.cs
@@ -19,21 +19,21 @@ namespace SIE.Dashboard
             {
                 dashboard.labels.Add("Atividades em construção");
                 acitiviesCount.Add(activities.Count(r => r.CurrentState == (int)EActivityState.Building));
-                acitivitiesColor.Add("#139A43");
+                acitivitiesColor.Add("#2CEAA3");
             }
 
             if (activities.Any(r => r.CurrentState == (int)EActivityState.InProgress))
             {
                 dashboard.labels.Add("Atividades em andamento");
                 acitiviesCount.Add(activities.Count(r => r.CurrentState == (int)EActivityState.InProgress));
-                acitivitiesColor.Add("#3454D1");
+                acitivitiesColor.Add("#333333");
             }
 
             if (activities.Any(r => r.CurrentState == (int)EActivityState.Done))
             {
                 dashboard.labels.Add("Atividades finalizadas");
                 acitiviesCount.Add(activities.Count(r => r.CurrentState == (int)EActivityState.Done));
-                acitivitiesColor.Add("#F2DC5D");
+                acitivitiesColor.Add("#0E3F1D");
             }
 
             dashboard.datasets.Add(new MDashboardDatasets
