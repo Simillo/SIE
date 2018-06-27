23b2e33d84771a645c3bd483c2d1e87ecfcb84a6
Simillo Nakai <simillonakai@gmail.com>
2018-06-26 23:55:52 -0300

    alguns unit test

--- SIE.Tests/Controllers/PersonControllerTest.cs
+++ SIE.Tests/Controllers/PersonControllerTest.cs
@@ -14,15 +14,9 @@ namespace SIE.Tests.Controllers
 {
     public class PersonControllerTest
     {
-        public PersonControllerTest()
-        {
-            InitContext();
-        }
-
         private SIEContext _context;
         private IConfiguration _configuration;
-
-        public void InitContext()
+        public PersonControllerTest()
         {
             var builder = new DbContextOptionsBuilder<SIEContext>().UseInMemoryDatabase();
             _configuration = new ConfigurationBuilder().Build();
@@ -36,19 +30,207 @@ namespace SIE.Tests.Controllers
                 Email = "simillonakai@gmail.com",
                 Name = "Pessoa teste",
                 Password = "619bf74a84a52a1cb50a025654076dceb92a911c8929d8a9aec158c35ae359db",
-                Profile = (int) EProfile.Teacher,
+                Profile = (int)EProfile.Teacher,
                 Sex = 1
             };
             _context.Person.Add(person);
             _context.SaveChanges();
         }
 
+        #region Save (teacher, saving)
+        [Fact]
+        public void TestSaveConsigoCadastrarUmProfessorSemInstituicao()
+        {
+            var mPerson = new MPerson
+            {
+                Id = 2,
+                BirthDate = DateTime.Now,
+                Institution = null,
+                Cpf = "84733187491",
+                Email = "email@do.professor.com",
+                Name = "Nome professor",
+                Password = "123123a",
+                Profile = (int)EProfile.Teacher,
+                Sex = 1
+            };
+
+            var controller = new PersonController(_context, _configuration);
+            var res = controller.Save(mPerson) as ObjectResult;
+
+            Assert.NotNull(res);
+            Assert.Equal((int)HttpStatusCode.OK, res.StatusCode);
+            Assert.Equal("Cadastro realizado com sucesso!", ((MResponseContent)res.Value).message);
+        }
+
+        [Fact]
+        public void TestSaveConsigoCadastrarUmProfessorComInstituicao()
+        {
+            var mPerson = new MPerson
+            {
+                Id = 2,
+                BirthDate = DateTime.Now,
+                Institution = new MInstitution
+                {
+                    Name = "UFLA"
+                },
+                Cpf = "84733187491",
+                Email = "email@do.professor.com",
+                Name = "Nome professor",
+                Password = "123123a",
+                Profile = (int)EProfile.Teacher,
+                Sex = 1
+            };
+
+            var controller = new PersonController(_context, _configuration);
+            var res = controller.Save(mPerson) as ObjectResult;
+
+            Assert.NotNull(res);
+            Assert.Equal((int)HttpStatusCode.OK, res.StatusCode);
+            Assert.Equal("Cadastro realizado com sucesso!", ((MResponseContent)res.Value).message);
+        }
+
+        [Fact]
+        public void TestSaveConsigoCadastrarUmProfessorComInstituicaoJaExistente()
+        {
+            var institution = new Institution
+            {
+                Id = 1,
+                Name = "UFLA"
+            };
+            _context.Institution.Add(institution);
+            _context.SaveChanges();
+
+            var mPerson = new MPerson
+            {
+                Id = 2,
+                BirthDate = DateTime.Now,
+                Institution = new MInstitution
+                {
+                    Name = "UFLA"
+                },
+                Cpf = "84733187491",
+                Email = "email@do.professor.com",
+                Name = "Nome professor",
+                Password = "123123a",
+                Profile = (int)EProfile.Teacher,
+                Sex = 1
+            };
+
+            var controller = new PersonController(_context, _configuration);
+            var res = controller.Save(mPerson) as ObjectResult;
+
+            Assert.NotNull(res);
+            Assert.Equal((int)HttpStatusCode.OK, res.StatusCode);
+            Assert.Equal("Cadastro realizado com sucesso!", ((MResponseContent)res.Value).message);
+        }
+        #endregion
+
+        #region Save (person, errors)
+        [Fact]
+        public void TestSaveNaoConsigoCadastrarUmaPessoaComEmailExistente()
+        {
+            var mPerson = new MPerson
+            {
+                Id = 2,
+                BirthDate = DateTime.Now,
+                Institution = null,
+                Cpf = "84733187491",
+                Email = "simillonakai@gmail.com",
+                Name = "Nome professor",
+                Password = "123123a",
+                Profile = (int)EProfile.Teacher,
+                Sex = 1
+            };
+
+            var controller = new PersonController(_context, _configuration);
+            var res = controller.Save(mPerson) as ObjectResult;
+
+            Assert.NotNull(res);
+            Assert.Equal((int)HttpStatusCode.BadRequest, res.StatusCode);
+            Assert.Equal("Campo(s) inválido(s)!", ((MResponseContent)res.Value).message);
+        }
+
+        [Fact]
+        public void TestSaveNaoConsigoCadastrarUmaPessoaComEmailInvalido()
+        {
+            var mPerson = new MPerson
+            {
+                Id = 2,
+                BirthDate = DateTime.Now,
+                Institution = null,
+                Cpf = "84733187491",
+                Email = "e-mailMuitoInválido",
+                Name = "Nome professor",
+                Password = "123123a",
+                Profile = (int)EProfile.Teacher,
+                Sex = 1
+            };
+
+            var controller = new PersonController(_context, _configuration);
+            var res = controller.Save(mPerson) as ObjectResult;
+
+            Assert.NotNull(res);
+            Assert.Equal((int)HttpStatusCode.BadRequest, res.StatusCode);
+            Assert.Equal("Campo(s) inválido(s)!", ((MResponseContent)res.Value).message);
+        }
+
+        [Fact]
+        public void TestSaveNaoConsigoCadastrarUmaPessoaComCpfInvalido()
+        {
+            var mPerson = new MPerson
+            {
+                Id = 2,
+                BirthDate = DateTime.Now,
+                Institution = null,
+                Cpf = "123",
+                Email = "email@ok.com",
+                Name = "Nome professor",
+                Password = "123123a",
+                Profile = (int)EProfile.Teacher,
+                Sex = 1
+            };
+
+            var controller = new PersonController(_context, _configuration);
+            var res = controller.Save(mPerson) as ObjectResult;
+
+            Assert.NotNull(res);
+            Assert.Equal((int)HttpStatusCode.BadRequest, res.StatusCode);
+            Assert.Equal("Campo(s) inválido(s)!", ((MResponseContent)res.Value).message);
+        }
+        [Fact]
+        public void TestSaveNaoConsigoCadastrarUmaPessoaComSenhaInvalida()
+        {
+            var mPerson = new MPerson
+            {
+                Id = 2,
+                BirthDate = DateTime.Now,
+                Institution = null,
+                Cpf = "84733187491",
+                Email = "email@ok.com",
+                Name = "Nome professor",
+                Password = "11",
+                Profile = (int)EProfile.Teacher,
+                Sex = 1
+            };
+
+            var controller = new PersonController(_context, _configuration);
+            var res = controller.Save(mPerson) as ObjectResult;
+
+            Assert.NotNull(res);
+            Assert.Equal((int)HttpStatusCode.BadRequest, res.StatusCode);
+            Assert.Equal("Campo(s) inválido(s)!", ((MResponseContent)res.Value).message);
+        }
+        #endregion
+
+        #region GetInfoByToken (Errors)
         [Fact]
         public void TestGetInfoByTokenNaoExistente()
         {
-            var token = "Token não existente";
+            const string token = "Token não existente";
+
             var controller = new PersonController(_context, _configuration);
             var res = controller.GetInfoByToken(token) as ObjectResult;
+
             Assert.NotNull(res);
             Assert.Equal((int)HttpStatusCode.Unauthorized, res.StatusCode);
             Assert.Equal("Essa solicitação não existe!", ((MResponseContent)res.Value).message);
@@ -57,7 +239,7 @@ namespace SIE.Tests.Controllers
         [Fact]
         public void TestGetInfoByTokenNaoAtiva()
         {
-            var token = "Token expirado";
+            const string token = "Token expirado";
 
             var passwordRecovery = new PasswordRecovery
             {
@@ -73,9 +255,12 @@ namespace SIE.Tests.Controllers
 
             var controller = new PersonController(_context, _configuration);
             var res = controller.GetInfoByToken(token) as ObjectResult;
+
             Assert.NotNull(res);
             Assert.Equal((int)HttpStatusCode.Unauthorized, res.StatusCode);
             Assert.Equal("Essa solicitação já expirou!", ((MResponseContent)res.Value).message);
         }
+        #endregion
+
     }
 }
