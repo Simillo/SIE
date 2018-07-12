b15b6e76e3184c0102845c2438048fd647eee4f5
Simillo Nakai <simillonakai@gmail.com>
2018-07-11 21:30:45 -0300

    Correções de portguês

--- Interface/src/components/teacher/Activity.vue
+++ Interface/src/components/teacher/Activity.vue
@@ -110,7 +110,7 @@
               :fileName='"atividade"',
               :canUpload='false',
               :files='activity.Attachments',
-              :title='"Arquivo(s) adicionado pelo aluno"'
+              :title='"Arquivo(s) adicionado(s) pelo aluno"'
             )
 </template>
 
@@ -184,7 +184,7 @@ export default {
           ExpirationDate: this.activity.ExpirationDate
         }
         this.files = this.activity.Uploads
-        this.loadedFiles = true
+        this.loadedFiles = this.activity.Uploads.length > 0
         this.searched = this.activity.Answers.map(a => {
           a.Open = false
           a.loadedAttachments = a.Attachments.length > 0
